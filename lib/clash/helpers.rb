e8245c13996f619e68ce15ea2884dc329d69ac4d
Parker Moore <parkrmoore@gmail.com>
2015-01-08 23:54:39 -0800

    Direct the output of the commands to /dev/null unless the DEBUG flag is on.

--- lib/clash/helpers.rb
+++ lib/clash/helpers.rb
@@ -35,6 +35,14 @@ module Clash
       end
     end
 
+    def system(*cmd)
+      Kernel.system("#{cmd.join(' ')} > #{output_file}")
+    end
+
+    def output_file
+      @output_file ||= (ENV['DEBUG'] ? '/dev/stdout' : '/dev/null')
+    end
+
     # Print a single character without a newline
     #
     def pout(str)
--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,6 @@
 require 'clash'
 
-count = Clash::Tests.new().tests.size
+count = Clash::Tests.new.tests.size
 FileUtils.mkdir_p('_output')
 
 puts "Running #{count} tests..."
@@ -15,8 +15,8 @@ end
 # This allows tests to pass on different systems
 %w{1 2}.each do |f|
   content = File.open("_output/#{f}").read
-  content = content.gsub(/Configuration file: .+\//, 'Configuration file: ') 
-    .gsub(/Source: .+\//, 'Source: ') 
+  content = content.gsub(/Configuration file: .+\//, 'Configuration file: ')
+    .gsub(/Source: .+\//, 'Source: ')
     .gsub(/Destination: .+\//, 'Destination: ')
 
   File.open("_output/#{f}", 'w') { |f| f.write(content) }
